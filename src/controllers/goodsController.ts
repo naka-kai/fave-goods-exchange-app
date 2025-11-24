529a00daa8fd0c99bcc20c3895dd731db3813b63
kai <sea.k.naka@gmail.com>
2025-11-24 16:08:23 +0900

    シリーズIDで取得

--- src/controllers/goodsController.ts
+++ src/controllers/goodsController.ts
@@ -2,7 +2,34 @@ import Express from 'express';
 import goodsList from '../data/goods.js';
 
 export default {
+  // 全シリーズ
   index: (req: Express.Request, res: Express.Response) => {
     res.status(200).send(goodsList);
+  },
+
+  // 各シリーズごと
+  showSeries: (req: Express.Request, res: Express.Response) => {
+    // 渡ってきたシリーズID
+    const seriesId = req.params.seriesId;
+
+    // シリーズIDの存在チェック
+    if (!seriesId) {
+      return res.status(400).send({
+        message: 'シリーズIDが指定されていません。',
+      });
+    }
+
+    // シリーズIDの存在チェック
+    const seriesList = goodsList.find((series) => series.id === seriesId);
+
+    // シリーズが見つからなかった場合
+    if (seriesList == null) {
+      return res.status(404).send({
+        message: 'このシリーズは見つかりませんでした。',
+      });
+    }
+
+    // 各シリーズのデータを返却
+    res.status(200).send(seriesList);
   },
 };
--- src/routes/goodsRoutes.ts
+++ src/routes/goodsRoutes.ts
@@ -4,5 +4,6 @@ import goodsController from '../controllers/goodsController.js';
 const router = Express.Router();
 
 router.get('/goods', goodsController.index);
+router.get('/goods/:seriesId', goodsController.showSeries);
 
 export default router;
